bd8e43c52e8ccff184b095253e4c71c8a061a39a
sumin-Kim-00 <ksm6399@gmail.com>
2022-12-28 10:50:24 +0900

    variables 학습

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,7 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        // 변수 선언만 하고 싶을 경우(할당안해도 오류 X)
+        "no-unused-vars":'off'
     }
 }
---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,48 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+
+/* 1번 */
+let admin;
+let name;
+
+name = "John";
+admin = name;
+console.log(admin);
+
+/* 2번 */
+let ourPlanetName = "Earth";
+let currentUserName = "sumin";
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+// const productPrice = 1000;
+let calcProductQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+
+// - 구매 결제 여부
+let isPayment;
+// 했냐 안했냐 - is, 가지고 있냐 없냐 - has
+let hasClassName;
+
+// - 구매 결제 내역
+let paymentHistory;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = "APPLE";
+
+// - 오늘의 운세
+let todayHoroscope;
+
+// - 상품 정보
+let productInfo = "iPhone";
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
