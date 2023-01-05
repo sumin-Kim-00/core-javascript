3907247a8e5807a0052b85fe77b80c07cc6cb409
sumin-Kim-00 <ksm6399@gmail.com>
2023-01-05 11:37:45 +0900

    Closure 학습

---
+++ client/chapter/core/12-2.closure.js
@@ -0,0 +1,25 @@
+
+
+
+// function earth() {
+
+//   function tiger
+
+// }
+
+
+function handler() {
+  let isClicked = false;
+
+  return function () {
+    if (isClicked === true) {
+      this.style.background = "transparent";
+    } else {
+      this.style.background = "red";
+    }
+
+    isClicked = !isClicked;
+  };
+}
+
+document.querySelector(".first").addEventListener("click", handler());
