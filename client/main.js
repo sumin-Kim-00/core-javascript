c67f83270e5a6c851ea861e2d5c48a1cde9800cd
sumin-Kim-00 <ksm6399@gmail.com>
2023-01-13 11:01:22 +0900

    함수형 프로그래밍_계산기 만들기

--- client/main.js
+++ client/main.js
@@ -1,2 +1,61 @@
 // 웹 브라우저 환경
-console.log(window);
+/* eslint no-undef:'warn' */
+/* eslint no-unused-vars: 'off' */
+
+// console.log(window);
+
+const firstInput = getNode('#firstNumber');
+const secondInput = getNode('#secondNumber');
+const done = getNode('#done');
+const result = getNode('.result');
+
+
+function getInputValue(node){
+  if(typeof node === 'string') node = getNode(node);
+  if(node.tagName !== 'INPUT') refError('getInputValue 함수는 INPUT ELEMENT만 허용합니다.')
+  return node.value;
+}
+
+const sum = (valueA, valueB) => valueA + valueB;
+
+function clearContents(node){
+  if(typeof node === 'string') node = getNode(node);
+  node.textContent = '';
+}
+
+
+function handler(e){
+  e.preventDefault();
+
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput)
+  let total = sum(firstValue, secondValue);
+  
+  // result.textContent = total;
+
+  clearContents(result);
+  insertLast(result, total);
+}
+
+
+function inputHandler(){
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput)
+  let total = sum(firstValue, secondValue);
+  
+  clearContents(result);
+  insertLast(result, total);
+}
+
+
+
+done.addEventListener('click', handler);
+
+// change 이벤트 - 값이 변경됐을 때 실시간으로 이벤트 발생
+firstInput.addEventListener('change', inputHandler);
+secondInput.addEventListener('change', inputHandler);
+
+
+
+
+// console.log(firstValue);
